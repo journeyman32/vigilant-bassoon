97c2ae4f57f3966bdf48718b201090f43208f138
Journeyman32 <bryan@coderbox.co>
2021-11-02 18:05:21 -0500

    Added events

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,36 +4,34 @@ import { CoursesService } from "./courses.service";
 @Component({
     selector: 'courses',
     template: `
-    Angular 
-    <h2>{{getTitle()}}</h2>
-    <ul>
-        <li *ngFor="let course of courses">
-        {{course}}</li>
-    </ul>
-    <img src="{{ imageUrl}}"/>
-    <img [src]="imageUrl"/>
-    <table>
-        <tr>
-            <td [attr.colSpan]="colSpan"></td>
-        </tr>
-    </table>
-
-    <button class="btn btn-primary" [class.active]="isActive">Save</button>
-
+    <div (click)="onDivClick()">
+    <button (click)="onSave($event)" [style.backgroundColor]="isActive ? 'blue' : 'white'">Save</button>
+</div>
     `
 })
 export class CoursesComponent{
     getTitle(){
         return "New list of courses";
     }
+
+    onSave($event: any){
+        $event.stopPropagation();
+        console.log($event);
+        console.log("Message that its logged");
+    }
+
+    onDivClick(){
+        console.log("div was clicked");
+    }
     courses;
     imageUrl = "http://lorempixel.com/400/200";
     colSpan = 2;
 
-    isActive = true;
+    isActive = 0;
 
     constructor(service: CoursesService){
 
         this.courses = service.getCourses();
     }
+
 }
