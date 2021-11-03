601f12f51eb98dceaa8a768549157661565ec185
Journeyman32 <bryan@coderbox.co>
2021-11-03 16:13:16 -0500

    Adding custom pipe

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,19 +3,14 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'courses',
     template: `
-
-    <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" />
+{{text | summary}}
 
     
     `
 })
 export class CoursesComponent{
  
-    email:any="example@bryan.com";
-
-    onKeyUp(){
-        console.log(this.email);
-    } 
+    text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     
 
    
