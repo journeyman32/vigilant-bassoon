4cb656b75ff96dd9f04831b80ab35e306da314f7
Journeyman32 <bryan@coderbox.co>
2021-11-14 13:09:19 -0600

    ngFor with exported props

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  viewMode = "something";
+  courses = [
+    {id:1, name: "Course1"},
+    {id:2, name: "Course2"},
+    {id:3, name: "Course3"},
+
+  ];
 }
 
