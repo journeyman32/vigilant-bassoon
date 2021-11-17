ae2720f8afebfa12f117f86e1887bf11b9857bcd
Journeyman32 <bryan@coderbox.co>
2021-11-17 12:07:13 -0600

    style before ngStyle and is messy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,29 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  courses;
-
-  post={
-    isFavorite:true
-  };
-
-  loadCourses(){
-    this.courses = [
-      {id:1, name: "Course1"},
-      {id:2, name: "Course2"},
-      {id:3, name: "Course3"},
-  
-    ];  
-  
-  }
-  
-  trackCourse(index, course){
-    return course ? course.id: undefined;
-  }
-
-  onFavoriteChanged(event){
-
-  }
+  canSave = true;
 
 }
 
