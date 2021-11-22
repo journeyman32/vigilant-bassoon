42a40febe4b1200574574b99afebd8890b1e2394
Journeyman32 <bryan@coderbox.co>
2021-11-21 18:36:01 -0600

    same attribute and custom directive

--- src/app/input-format.directive.ts
+++ src/app/input-format.directive.ts
@@ -4,7 +4,7 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
   selector: '[appInputFormat]'
 })
 export class InputFormatDirective {
-  @Input('format') format;
+  @Input('appInputFormat') format;
 
   constructor(private el: ElementRef) { }
 
