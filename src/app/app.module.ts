83194cabaf74768a46a26bf964111c462f9ef2a6
Journeyman32 <bryan@coderbox.co>
2021-11-24 22:29:42 -0600

    programatic form creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
 
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -29,7 +29,8 @@ import { SignupFormComponent } from './signup-form/signup-form.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [CoursesService],
   bootstrap: [AppComponent]
