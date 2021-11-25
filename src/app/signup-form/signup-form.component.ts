64743875a8b64f490b5724ff691bbe76420f21cf
Journeyman32 <bryan@coderbox.co>
2021-11-24 22:48:21 -0600

    redactive form validation more stream lined

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -12,5 +12,12 @@ export class SignupFormComponent {
     password: new FormControl('',Validators.required)
   });
 
+  get username(){
+    return this.form.get('username');
+  }
+
+  get password(){
+    return this.form.get('password');
+  }
 
 }
